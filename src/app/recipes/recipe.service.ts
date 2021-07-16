9d59c0d0f7f8416adc952bc0d3e8b1a10fbb19b4
Greedion <adrian43graf@gmail.com>
2021-07-16 08:45:22 +0200

    Finish Observable

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,10 @@
 import { ShoppingListService } from './../shopping-list/shopping-list.service';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-
     private recipes: Recipe[] = [
         new Recipe(
           'Tasty Schnitzel',
@@ -33,6 +31,10 @@ export class RecipeService {
 
       getRecipes() {
           return this.recipes.slice();
+      }
+
+      getRecipe(index: number){
+        return this.recipes[index];
       }
 
       addIngredientsToShoppingList(ingredients: Ingredient[]){
